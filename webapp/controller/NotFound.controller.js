7fb6a6c7ffe5f333f586e005b669a6be40662183
Paulo Silva <paulo.hendrixpss@gmail.com>
2022-10-10 05:27:10 +0000

    Fontes modulo 09 aula01

---
+++ webapp/controller/NotFound.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+    "moovi/m09a01/controller/BaseController"    
+ ], function (Controller) {
+    "use strict";
+    return Controller.extend("moovi.m09a01.controller.NotFound", {
+       onInit: function () {
+       }
+    });
+ });
